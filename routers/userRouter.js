76417cec1f5f6dcd6153f003a228079aee71a214
inegg <inegg.apps@gmail.com>
2019-04-12 23:20:20 +0900

    Completed Videos query.
    - Installed ESLint for checking error and arranging code.
    - Completed Edit in controller.
    - Completed Delete in controller.
    - Completed Search in controller.
    - Supplemented videoDetail pug page.
    
    ETC...
    
    It's time to learn the webpack!!!

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import routes from "../routes";
-import { userDetail, editProfile, changePassword, users } from "../controllers/userController";
+import { userDetail, editProfile, changePassword } from "../controllers/userController";
 const userRouter = express.Router();
 
 userRouter.get(routes.editProfile, editProfile);
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -5,19 +5,26 @@ import {
   getUpload,
   postUpload,
   videoDetail,
-  editvideo,
   deleteVideo,
-  videos
+  getEditVideo,
+  postEditVideo
 } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-//↑ routes.videoDetail is function. Therfore, when this method is called, it should be excuted!!! . -> videoDetail() 괄호 꼭! 명시
-videoRouter.get(routes.editVideo, editvideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+// ↑ routes.videoDetail is function. Therfore, when this method is called, it should be excuted!!! . -> videoDetail() 괄호 꼭! 명시
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
