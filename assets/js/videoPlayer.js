c7848f883deac2cce04b61e626545b2b8e422b51
inegg <inegg.apps@gmail.com>
2019-04-18 12:21:33 +0900

    Controllers of videoPlayer had been updated!
    Controllers of videoPlyaer had been updated!
    
    - Volume Up/Down Control
    - Play/Pause bug fix
    When video was done, Controller showed not pause button but play button.
    - Fullscreen bug fixed.
    When we click fullscreen button,  screen fulfilled monitor in all browser that implemented method each browser(moz, webkit, ms).
    
    ETC...

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,8 +1,21 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
-const volumeBtn = document.getElementById("jsVolumeButton");
 const fullScreenBtn = document.getElementById("jsFullScreen");
+const volumeBtn = document.getElementById("jsVolumeButton");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+const volumeRange = document.getElementById("jsVolume");
+
+function changeVolumeUI(volume) {
+  if (volume >= 0.6) {
+    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else if (volume >= 0.3) {
+    volumeBtn.innerHTML = '<i class="fas fa-volume-down"></i>';
+  } else if (volume < 0.1) {
+    volumeBtn.innerHTML = '<i class="fas fa-volume-off"></i>';
+  }
+}
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -15,31 +28,97 @@ function handlePlayClick() {
 }
 
 function handleVolumeClick() {
-  videoPlayer.muted = !videoPlayer.muted;
   if (videoPlayer.muted) {
-    volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+    videoPlayer.muted = false;
+    volumeRange.value = videoPlayer.volume;
+    changeVolumeUI(volumeRange.value);
   } else {
-    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+    videoPlayer.muted = true;
+    volumeRange.value = 0;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }
 }
 
 function exitFullScreen() {
   fullScreenBtn.innerHTML = '<i class="fas fa-expand"></i>';
   fullScreenBtn.addEventListener("click", goFullScreen);
-  document.webkitExitFullscreen();
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
 }
 
 function goFullScreen() {
-  videoContainer.webkitRequestFullscreen();
+  if (videoContainer.requestFullscreen) {
+    videoContainer.requestFullscreen();
+  } else if (videoContainer.mozRequestFullScreen) {
+    videoContainer.mozRequestFullScreen();
+  } else if (videoContainer.webkitRequestFullscreen) {
+    videoContainer.webkitRequestFullscreen();
+  } else if (videoContainer.msRequestFullscreen) {
+    videoContainer.msRequestFullscreen();
+  }
   fullScreenBtn.innerHTML = '<i class="fas fa-compress"></i>';
   fullScreenBtn.removeEventListener("click", goFullScreen);
   fullScreenBtn.addEventListener("click", exitFullScreen);
 }
 
+const formatDate = seconds => {
+  const secondsNumber = parseInt(seconds, 10);
+  let hours = Math.floor(secondsNumber / 3600);
+  let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+  let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (totalSeconds < 10) {
+    totalSeconds = `0${totalSeconds}`;
+  }
+  return `${hours}:${minutes}:${totalSeconds}`;
+};
+
+function setTotalTime() {
+  console.log(videoPlayer.duration);
+  const totalTimeString = formatDate(videoPlayer.duration);
+  totalTime.innerHTML = totalTimeString;
+  setInterval(getCurrentTime, 1000);
+}
+
+function getCurrentTime() {
+  currentTime.innerHTML = formatDate(Math.floor(videoPlayer.currentTime));
+}
+
+function handleEnded() {
+  videoPlayer.currentTime = 0;
+  playBtn.innerHTML = '<i class="fas fa-play"></i>';
+}
+
+function handleDrag(event) {
+  const {
+    target: { value }
+  } = event;
+  videoPlayer.volume = value;
+  changeVolumeUI(value);
+}
+
 function init() {
+  videoPlayer.volume = 0.5;
+  changeVolumeUI(videoPlayer.volume);
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
   fullScreenBtn.addEventListener("click", goFullScreen);
+  videoPlayer.addEventListener("loadedmetadata", setTotalTime);
+  videoPlayer.addEventListener("ended", handleEnded);
+  volumeRange.addEventListener("input", handleDrag);
 }
 
 if (videoContainer) {
